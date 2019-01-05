bddde4c1513b4dc1da4606501585fe0c14f6f0ce
Vincent <vincent.leguevel.sio@gmail.com>
2019-01-05 23:28:39 +0100

    Ajout modules gestion des sessions. configuration passport-local-strategy. renommage authROuter

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import session from "express-session";
+import passport from "passport";
 
 import NodeRouter from './routes/nodeRouter';
-import SignRouter from './routes/signRouter';
+import AuthRouter from './routes/authRouter';
 
 export default class Server {
 
@@ -31,11 +33,25 @@ export default class Server {
 
         this.app.use(bodyParser.urlencoded({extended : true}));
         this.app.use(bodyParser.json());
-        this.app.use(express.static(path.join(__dirname, "public")));
+        this.app.use('/static',express.static(path.join(__dirname, "public")));
 
+        this.app.use(session({
+            secret: 'secret',
+            resave: true,
+            saveUninitialized: true,
+        }));
+
+
+        //Initialisation passpot
+        this.app.use(passport.initialize());
+        this.app.use(passport.session());
+
+        this.app.use(passport.session());
+
+
+        this.app.set('env',process.env.NODE_ENV);
         this.app.set('views',path.join(__dirname,'../views'));
         this.app.set('view engine','pug');
-        this.app.set('env',process.env.NODE_ENV);
     }
 
     public start(): void {
@@ -48,7 +64,7 @@ export default class Server {
     private routes(): void {
         //Déclarer les diffentes routes
         this.app.use('/',NodeRouter);
-        this.app.use('/authentication',SignRouter);
+        this.app.use('/auth',AuthRouter);
 
         this.app.all("*", (req,res) => {
             res.send('404');
